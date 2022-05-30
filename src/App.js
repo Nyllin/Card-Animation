b67b2560c3635a4534296f1e22fb75c90fc27a9d
Nay Yi Lin <nayyi0608@gmail.com>
2022-05-30 19:32:33 +0630

    Card animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {motion} from 'framer-motion';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+ const [open,setOpen] = useState(false);
+ const clickHandler = () =>{
+   setOpen(!open);
+ }
+  // console.log(open);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div layout transition={{layout:{
+        duration : 1,type:'spring'
+      }}} style={{borderRadius:"1rem",boxShadow:"0px 10px 30px rgba(0.0.0.0.5)"}} onClick={clickHandler} className='card'>
+        <motion.h2 layout="position">Framer Motion 🚀</motion.h2>
+        {open &&
+        <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1}} className='expand'>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+            s standard dummy text ever 
+            since the 1500s. </p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </motion.div>
+        }
+      </motion.div>
     </div>
   );
 }
